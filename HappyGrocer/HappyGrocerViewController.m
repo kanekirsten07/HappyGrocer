2efe8a7d3aacbfd9a3dea194f1ab97a5d64ae590
Kirsten Kane <kanekirsten07@gmail.com>
2013-04-26 16:22:17 -0700

    Revert "Navigation Controller"
    This reverts commit dbeb5587681dcbfe4b557a46dee9011121d5896f.

---
+++ HappyGrocer/HappyGrocerViewController.m
@@ -0,0 +1,44 @@
+//
+//  HappyGrocerViewController.m
+//  HappyGrocer
+//
+//  Created by Kirsten Kane on 4/4/13.
+//  Copyright (c) 2013 Kirsten Kane. All rights reserved.
+//
+
+#import "HappyGrocerViewController.h"
+#import "AddItemViewController.h"
+#import "ItemViewController.h"
+
+@interface HappyGrocerViewController ()
+
+@end
+
+@implementation HappyGrocerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)AddItem:(id)sender {
+    AddItemViewController *AVC = [[AddItemViewController alloc]initWithNibName:@"AddItemViewController" bundle:nil];
+    
+    [self presentModalViewController:AVC animated:YES];
+}
+
+- (IBAction)ViewItem:(id)sender {
+    
+    ItemViewController *IVC =[[ItemViewController alloc]initWithStyle: UITableViewStylePlain];
+    [self presentModalViewController:IVC animated:YES];
+   
+}
+@end
